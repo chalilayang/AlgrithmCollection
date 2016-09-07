830d01a39add379e285f9e4f7809c1802fd2f28e
chalilayang <chalilayang@gmail.com>
2016-09-07 10:21:18 +0800

    first commit

---
+++ src/com/algrithm/main/MathTest.java
@@ -0,0 +1,14 @@
+package com.algrithm.main;
+
+public class MathTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        double result1 = Math.ceil(-0.09);
+        System.out.print(result1 + "\n");
+    }
+
+}
