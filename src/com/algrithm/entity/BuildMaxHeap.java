459d0181b2c7c8d08e8a61b0c46cbdb211f53478
chalilayang <chalilayang@gmail.com>
2016-10-26 17:41:51 +0800

    update

--- src/com/algrithm/entity/BuildMaxHeap.java
+++ src/com/algrithm/entity/BuildMaxHeap.java
@@ -28,34 +28,7 @@ public class BuildMaxHeap {
         // 从树的深层逆序，构造max-heap,正好每次均可满足
         // MaxHeapify算法的前提，即所有子二叉树已经是max-heap
         for (int i = heapSize / 2; i >= 0; i--) {
-            maxHeapify(input, i);
-        }
-    }
-
-    /**
-     * MaxHeap，调整算法，前提是假设所有的子二叉树已经是max-heap。
-     * 复杂度： 因为：T (n) ≤ T(2n/3) + Θ(1) 所以有：T (n) = O(lgn) 
-     * @param array 
-     * @param index
-     */
-    private static void maxHeapify(int[] array, int index) {
-        int l = index * 2 + 1;
-        int r = l + 1;
-        int largest;
-        // 如果左叶子节点索引小于堆大小，比较当前值和左叶子节点的值，取值大的索引值
-        if (l < heapSize && array[l] > array[index]) {
-            largest = l;
-        } else {
-            largest = index;
-        }
-        // 如果右叶子节点索引小于堆大小，比较右叶子节点和之前比较得出的较大值，取大的索引值
-        if (r < heapSize && array[r] > array[largest]) {
-            largest = r;
-        }
-        // 交换位置，并继续递归调用该方法调整位置。
-        if (largest != index) {
-            swap(array, index, largest);
-            maxHeapify(array, largest);
+            adjust(input, i, heapSize - 1);
         }
     }
     
@@ -80,7 +53,7 @@ public class BuildMaxHeap {
         }
     }
 
- // 对一个最大堆heap排序
+    // 对一个最大堆heap排序
     public static void heapSort(int[] heap) {
         for (int i = heap.length - 1; i > 0; i--) {
             swap(heap, 0, i);
